f0d9313892fd3cce293d13305bc8456808040cf5
Daniel Golle <daniel@makrotopia.org>
2022-01-30 19:36:34 +0000

    realtek: add image and device tree for ZYXEL XS1930 family
    dts from Sebastian Gottschall <s.gottschall@dd-wrt.com>'s pie-5.10 tree
    with fixed MTD partitions.
    Use default image generation code which works with stock loader for
    initramfs image.
    Add CONFIG_AQUANTIA_PHY=y to kernel config to enable driver for AQR813
    8-port PHY found as well as the AQR113 PHYs single port ones found in the
    -12 version of the device.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -1,1 +1,15 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
+define Device/zyxel_xs1930
+  KERNEL_LOADADDR = 0x80220000
+  KERNEL_ENTRY = 0x80220000
+  SOC := rtl9313
+  IMAGE_SIZE := 14336k
+  DEVICE_VENDOR := ZyXEL
+  DEVICE_MODEL := XS1930 series
+  ZYXEL_VERS := ABQF
+  DEVICE_PACKAGES := ip-bridge ethtool
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | gzip | zyxel-vers $$$$(ZYXEL_VERS) | \
+	uImage gzip
+endef
+TARGET_DEVICES += zyxel_xs1930
